d60330b9c1dd32dca1b98d11f1c8c489224370ad
serhatayata <srhtayata@gmail.com>
2022-09-03 16:31:44 +0300

    update config.cs

--- IdentityServer/EC.IdentityServer/Configuration/Config.cs
+++ IdentityServer/EC.IdentityServer/Configuration/Config.cs
@@ -7,15 +7,113 @@ namespace EC.IdentityServer.Configuration
     {
         public static IEnumerable<ApiResource> ApiResources => new ApiResource[]
         {
-            new ApiResource("resource_base"){Scopes={"base_fullpermission"}},
-            new ApiResource("resource_catalog"){Scopes={"catalog_fullpermission"}},
-            new ApiResource("resource_catalog"){Scopes={"catalog_fullpermission"}},
-            new ApiResource("resource_catalog"){Scopes={"catalog_fullpermission"}},
-            new ApiResource("resource_catalog"){Scopes={"catalog_fullpermission"}},
-            new ApiResource("resource_catalog"){Scopes={"catalog_fullpermission"}},
-            new ApiResource("resource_catalog"){Scopes={"catalog_fullpermission"}},
-            new ApiResource("resource_order"){Scopes={"order_fullpermission"}},
-            new ApiResource("resource_gateway"){Scopes={"gateway_fullpermission"}},
+            #region Base
+            new ApiResource("resource_base")
+            {
+                Scopes=
+                {
+                    "base_full",
+                    "base_read",
+                    "base_write"
+                },
+                //Bu kısmı test et...
+                ApiSecrets =
+                {
+                    new Secret("base_secret".Sha256())
+                }
+            },
+	        #endregion
+            #region Basket
+            new ApiResource("resource_basket")
+            {
+                Scopes=
+                {
+                    "basket_full",
+                    "basket_read",
+                    "basket_write"
+                },
+                ApiSecrets =
+                {
+                    new Secret("basket_secret".Sha256())
+                }
+            },
+	        #endregion
+            #region Category
+            new ApiResource("resource_category")
+            {
+                Scopes=
+                {
+                    "category_full",
+                    "category_read",
+                    "category_write"
+                },
+                ApiSecrets =
+                {
+                    new Secret("category_secret".Sha256())
+                }
+            },
+	        #endregion
+            #region Discount
+            new ApiResource("resource_discount")
+            {
+                Scopes=
+                {
+                    "discount_full",
+                    "discount_read",
+                    "discount_write"
+                },
+                ApiSecrets =
+                {
+                    new Secret("discount_secret".Sha256())
+                }
+            },
+	        #endregion
+            #region Order
+            new ApiResource("resource_order")
+            {
+                Scopes=
+                {
+                    "order_full",
+                    "order_read",
+                    "order_write"
+                },
+                ApiSecrets =
+                {
+                    new Secret("order_secret".Sha256())
+                }
+            },
+	        #endregion
+            #region Payment
+            new ApiResource("resource_payment")
+            {
+                Scopes=
+                {
+                    "payment_full",
+                    "payment_read",
+                    "payment_write"
+                },
+                ApiSecrets =
+                {
+                    new Secret("payment_secret".Sha256())
+                }
+            },
+	        #endregion
+            #region Product
+            new ApiResource("resource_product")
+            {
+                Scopes=
+                {
+                    "product_full",
+                    "product_read",
+                    "product_write"
+                },
+                ApiSecrets =
+                {
+                    new Secret("product_secret".Sha256())
+                }
+            },
+	        #endregion
+
             new ApiResource(IdentityServerConstants.LocalApi.ScopeName)
         };
 
@@ -32,47 +130,47 @@ namespace EC.IdentityServer.Configuration
             new ApiScope[]
             {
                 #region Base
-                new ApiScope("base_fullpermission","Full permission for Base API"),
+                new ApiScope("base_full","Full permission for Base API"),
                 new ApiScope("base_read","Read permission for Base API"),
                 new ApiScope("base_write","Write permission for Base API"),
 	            #endregion
                 #region Basket
-                new ApiScope("basket_fullpermission","Full permission for Basket API"),
+                new ApiScope("basket_full","Full permission for Basket API"),
                 new ApiScope("basket_read","Read permission for Basket API"),
                 new ApiScope("basket_write","Write permission for Basket API"),
 	            #endregion
                 #region Category
-                new ApiScope("category_fullpermission","Full permission for Category API"),
+                new ApiScope("category_full","Full permission for Category API"),
                 new ApiScope("category_read","Read permission for Category API"),
                 new ApiScope("category_write","Write permission for Category API"),
 	            #endregion
                 #region Discount
-                new ApiScope("discount_fullpermission","Full permission for Discount API"),
+                new ApiScope("discount_full","Full permission for Discount API"),
                 new ApiScope("discount_read","Read permission for Discount API"),
                 new ApiScope("discount_write","Write permission for Discount API"),
 	            #endregion
                 #region Order
-                new ApiScope("order_fullpermission","Full permission for Order API"),
+                new ApiScope("order_full","Full permission for Order API"),
                 new ApiScope("order_read","Read permission for Order API"),
                 new ApiScope("order_write","Write permission for Order API"),
 	            #endregion
                 #region Payment
-                new ApiScope("payment_fullpermission","Full permission for Payment API"),
+                new ApiScope("payment_full","Full permission for Payment API"),
                 new ApiScope("payment_read","Read permission for Payment API"),
                 new ApiScope("payment_write","Write permission for Payment API"),
 	            #endregion
                 #region Product
-                new ApiScope("product_fullpermission","Full permission for Product API"),
+                new ApiScope("product_full","Full permission for Product API"),
                 new ApiScope("product_read","Read permission for Product API"),
                 new ApiScope("product_write","Write permission for Product API"),
 	            #endregion
                 #region Gateway
-                new ApiScope("gateway_fullpermission","Full permission for Gateway API"),
+                new ApiScope("gateway_full","Full permission for Gateway API"),
                 new ApiScope("gateway_read","Read permission for Gateway API"),
                 new ApiScope("gateway_write","Write permission for Gateway API"),
 	            #endregion
                 #region WebUI
-                new ApiScope("webui_fullpermission","Full permission for WebUI"),
+                new ApiScope("webui_full","Full permission for WebUI"),
                 new ApiScope("webui_read","Read permission for WebUI"),
                 new ApiScope("webui_write","Write permission for WebUI"),
 	            #endregion
@@ -137,36 +235,6 @@ namespace EC.IdentityServer.Configuration
                     AccessTokenLifetime = 60*60*2, // 2 hours
                     IdentityTokenLifetime= 60*60*2 // 2 hours
                 },
-                new Client
-                {
-                    ClientId = "basketswaggerui",
-                    ClientName = "Basket Swagger UI",
-                    AllowedGrantTypes = GrantTypes.Implicit,
-                    AllowAccessTokensViaBrowser = true,
-
-                    RedirectUris = { $"{clientsUrl["BasketApi"]}/swagger/oauth2-redirect.html" },
-                    PostLogoutRedirectUris = { $"{clientsUrl["BasketApi"]}/swagger/" },
-
-                    AllowedScopes =
-                    {
-                        "basket"
-                    }
-                },
-                new Client
-                {
-                    ClientId = "orderingswaggerui",
-                    ClientName = "Ordering Swagger UI",
-                    AllowedGrantTypes = GrantTypes.Implicit,
-                    AllowAccessTokensViaBrowser = true,
-
-                    RedirectUris = { $"{clientsUrl["OrderingApi"]}/swagger/oauth2-redirect.html" },
-                    PostLogoutRedirectUris = { $"{clientsUrl["OrderingApi"]}/swagger/" },
-
-                    AllowedScopes =
-                    {
-                        "orders"
-                    }
-                }
             };
         }
 
