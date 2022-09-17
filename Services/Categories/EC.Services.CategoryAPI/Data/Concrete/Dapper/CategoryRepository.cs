75ac1b15a0083722b38fdada28fa452013d39285
serhatayata <srhtayata@gmail.com>
2022-09-17 07:41:13 +0300

    CategoryRepository added

--- Services/Categories/EC.Services.CategoryAPI/Data/Concrete/Dapper/CategoryRepository.cs
+++ Services/Categories/EC.Services.CategoryAPI/Data/Concrete/Dapper/CategoryRepository.cs
@@ -1,48 +1,151 @@
-﻿using EC.Services.CategoryAPI.Data.Abstract.Dapper;
+﻿using Dapper;
+using EC.Services.CategoryAPI.Data.Abstract.Dapper;
 using EC.Services.CategoryAPI.Entities;
+using Microsoft.Data.SqlClient;
+using Nest;
+using System.ComponentModel.Design;
+using System.Drawing.Printing;
 
 namespace EC.Services.CategoryAPI.Data.Concrete.Dapper
 {
     public class CategoryRepository : ICategoryRepository
     {
-        public Task<bool> AnyAsync()
+        private readonly IConfiguration _configuration;
+
+        public CategoryRepository(IConfiguration configuration)
         {
-            throw new NotImplementedException();
+            _configuration = configuration;
         }
 
-        public Task<bool> AnyByLinkAsync(string link)
+        #region AnyAsync
+        public async Task<bool> AnyAsync()
         {
-            throw new NotImplementedException();
+            var sql = "SELECT TOP 1 * FROM Categories";
+            using (var connection = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+            {
+                connection.Open();
+                var result = await connection.QueryAsync<Category>(sql, new { Node = 0 });
+                if (result.Count() > 0)
+                {
+                    return true;
+                }
+                return false;
+            }
         }
+        #endregion
+        #region AnyByLinkAsync
+        public async Task<bool> AnyByLinkAsync(string link)
+        {
+            var sql = "SELECT TOP 1 * FROM Categories WHERE Link=@Link";
+            using (var connection = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+            {
+                connection.Open();
+                var result = await connection.QueryAsync<Category>(sql, new { Link = link });
+                if (result.Count() > 0)
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+        #endregion
+        #region GetLastLineAsync
+        public async Task<Category> GetLastLineAsync(int upperCategoryId)
+        {
+            var sql = "SELECT * FROM Categories WHERE ParentId=@ParentId ORDER BY Id DESC";
+            using (var connection = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+            {
+                connection.Open();
+                var result = await connection.QuerySingleOrDefaultAsync<Category>(sql, new { ParentId=upperCategoryId });
+                return result;
+            }
+        }
+        #endregion
+        #region GetAllAsync
+        public async Task<List<Category>> GetAllAsync()
+        {
+            var sql = "SELECT * FROM Categories";
+            using (var connection = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+            {
+                connection.Open();
+                var result = await connection.QueryAsync<Category>(sql);
+                return result.ToList();
+            }
+        }
+        #endregion
+        #region GetAllSubCategoriesByIdAsync
+        public async Task<List<Category>> GetAllSubCategoriesByIdAsync(int id)
+        {
+            var sql = "SELECT * FROM Categories WHERE ParentId=@ParentId";
+            using (var connection = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+            {
+                connection.Open();
+                var result = await connection.QueryAsync<Category>(sql, new { ParentId = id });
+                return result.ToList();
+            }
+        }
+        #endregion
+        #region GetAllSubCategoriesByIdPagingAsync
+        public async Task<List<Category>> GetAllSubCategoriesByIdPagingAsync(int id, int page = 1, int pageSize = 8)
+        {
+            var sql = "SELECT * FROM Categories WHERE ParentId=@ParentId ORDER BY Id OFFSET @Page * @PageSize ROWS FETCH NEXT @PageSize ROWS ONLY";
+            using (var connection = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+            {
+                connection.Open();
+                var result = await connection.QueryAsync<Category>(sql, new { ParentId = id, Page=page, PageSize=pageSize });
+                return result.ToList();
+            }
+        }
+        #endregion
+        #region GetByIdAsync
+        public async Task<Category> GetByIdAsync(int id)
+        {
+            var sql = "SELECT * FROM Categories WHERE Id=@Id";
+            using (var connection = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+            {
+                connection.Open();
+                var result = await connection.QuerySingleOrDefaultAsync<Category>(sql, new { Id = id });
+                return result;
+            }
+        }
+        #endregion
+        #region GetByLinkAsync
+        public async Task<Category> GetByLinkAsync(string link)
+        {
+            var sql = "SELECT * FROM Categories WHERE Link=@Link";
+            using (var connection = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+            {
+                connection.Open();
+                var result = await connection.QuerySingleOrDefaultAsync<Category>(sql, new { Link = link });
+                return result;
+            }
+        }
+        #endregion
+        #region GetByNameAsync
+        public async Task<List<Category>> GetByNameAsync(string name)
+        {
+            var sql = "SELECT * FROM Categories WHERE Name LIKE @Name";
+            using (var connection = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+            {
+                connection.Open();
+                var result = await connection.QueryAsync<Category>(sql, new { Name= $"%{name}%" });
+                return result.ToList();
+            }
+        }
+        #endregion
+        #region GetByNamePagingAsync
+        public async Task<List<Category>> GetByNamePagingAsync(string name, int page = 1, int pageSize = 8)
+        {
+            var sql = "SELECT * FROM Categories WHERE Name LIKE @Name ORDER BY Id OFFSET @Page * @PageSize ROWS FETCH NEXT @PageSize ROWS ONLY";
+            using (var connection = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+            {
+                connection.Open();
+                var result = await connection.QueryAsync<Category>(sql, new { Name = $"%{name}%", Page=page, PageSize=pageSize });
+                return result.ToList();
+            }
+        }
+        #endregion
 
-        public Task<List<Category>> GetAllAsync()
-        {
-            throw new NotImplementedException();
-        }
 
-        public Task<Category> GetByIdAsync(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<Category> GetByLinkAsync(string link)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<List<Category>> GetByNameAsync(string name)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<List<Category>> GetByNamePagingAsync(string name, int page = 1, int pageSize = 8)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<Category> GetLastLineAsync(int upperCategoryId)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
