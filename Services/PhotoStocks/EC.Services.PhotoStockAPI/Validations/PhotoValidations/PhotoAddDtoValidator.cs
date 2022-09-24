19d0cd6ef3e4e5c140a539b45d44f4861248e45e
serhatayata <srhtayata@gmail.com>
2022-09-24 11:27:37 +0300

    update

--- Services/PhotoStocks/EC.Services.PhotoStockAPI/Validations/PhotoValidations/PhotoAddDtoValidator.cs
+++ Services/PhotoStocks/EC.Services.PhotoStockAPI/Validations/PhotoValidations/PhotoAddDtoValidator.cs
@@ -13,6 +13,7 @@ namespace EC.Services.PhotoStockAPI.Validations.PhotoValidations
 
 
 
+
             //RuleFor(x => x.Name).NotEmpty().WithMessage(MessageExtensions.ErrorNotEmpty(CategoryEntities.CategoryName));
             //RuleFor(x => x.Name).NotNull().WithMessage(MessageExtensions.ErrorNotNull(CategoryEntities.CategoryName));
             //RuleFor(x => x.Name).Length(2, 50).WithMessage(MessageExtensions.ErrorBetween(CategoryEntities.CategoryName, 2, 50));
