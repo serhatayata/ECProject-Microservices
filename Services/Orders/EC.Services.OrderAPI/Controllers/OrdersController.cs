45d4802e775330fc661359c07cfeb9394045e4e8
Serhat <srhtayata@gmail.com>
2022-10-23 13:39:08 +0300

    Order service - NuGet packages - files and classes added

---
+++ Services/Orders/EC.Services.OrderAPI/Controllers/OrdersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EC.Services.OrderAPI.Controllers
+{
+    [Route("order/api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+
+    }
+}
