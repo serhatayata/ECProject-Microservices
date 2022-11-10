396b7bcdce011a43154790d6617a002d80efba30
Serhat <srhtayata@gmail.com>
2022-11-10 23:26:39 +0300

    LangResource Dapper methods added

--- Services/LangResources/EC.Services.LangResourceAPI/Data/Abstract/Dapper/IDapperLangResourceRepository.cs
+++ Services/LangResources/EC.Services.LangResourceAPI/Data/Abstract/Dapper/IDapperLangResourceRepository.cs
@@ -1,10 +1,16 @@
 ﻿using Core.DataAccess.Dapper;
+using Core.Dtos;
+using Core.Utilities.Results;
+using EC.Services.LangResourceAPI.Dtos.LangResourceDtos;
 using EC.Services.LangResourceAPI.Entities;
 
 namespace EC.Services.LangResourceAPI.Data.Abstract.Dapper
 {
     public interface IDapperLangResourceRepository:IGenericRepository<LangResource>
     {
+        Task<List<LangResource>> GetAllPagingAsync(PagingDto model);
+        Task<List<LangResource>> GetAllByLangIdPagingAsync(LangResourceGetAllByLangIdPagingDto model);
 
+        Task<LangResource> GetByMessageCodeAsync(string messageCode);
     }
 }
--- Services/LangResources/EC.Services.LangResourceAPI/Data/Concrete/Dapper/DapperLangRepository.cs
+++ Services/LangResources/EC.Services.LangResourceAPI/Data/Concrete/Dapper/DapperLangRepository.cs
@@ -1,9 +1,181 @@
-﻿using EC.Services.LangResourceAPI.Data.Abstract.Dapper;
+﻿using Core.Dtos;
+using Core.Utilities.Results;
+using Dapper;
+using EC.Services.LangResourceAPI.Data.Abstract.Dapper;
+using EC.Services.LangResourceAPI.Entities;
+using Elasticsearch.Net;
+using Microsoft.Data.SqlClient;
+using Nest;
+using System.Drawing.Printing;
 
 namespace EC.Services.LangResourceAPI.Data.Concrete.Dapper
 {
-    public class DapperLangRepository:IDapperLangRepository
+    public class DapperLangRepository : IDapperLangRepository
     {
+        private readonly IConfiguration _configuration;
+        private string _defaultConnection;
+
+        public DapperLangRepository(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _defaultConnection = _configuration.GetConnectionString("DefaultConnection");
+        }
+
+        #region AnyAsync
+        public async Task<bool> AnyAsync()
+        {
+            var sql = "SELECT TOP 1 * FROM Langs";
+            using (var connection = new SqlConnection(_defaultConnection))
+            {
+                connection.Open();
+                var result = await connection.QueryAsync<Lang>(sql);
+                if (result.Count() > 0)
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+        #endregion
+        #region GetAllAsync
+        public async Task<List<Lang>> GetAllAsync()
+        {
+            var sql = "SELECT l.Id,l.Name,l.Code, " +
+                      "lr.Id,lr.Tag,lr.Description,lr.MessageCode,lr.LangId " +
+                      "FROM Langs l LEFT JOIN LangResources lr " +
+                      "ON l.Id = lr.LangId";
+
+
+            var dict = new Dictionary<int, Lang>();
+
+            using (var connection = new SqlConnection(_defaultConnection))
+            {
+                connection.Open();
+                var result = await connection.QueryAsync<Lang, LangResource, Lang>(sql,
+                    (l, lr) =>
+                    {
+                        Lang lang;
+                        if (!dict.TryGetValue(l.Id, out lang))
+                        {
+                            lang = l;
+                            lang.LangResources = new List<LangResource>();
+                            dict.Add(l.Id, lang);
+                        }
+                        if (lr.LangId > 0)
+                        {
+                            l.LangResources.Add(lr);
+                        }
+                        return lang;
+                    }, splitOn: "Id");
+                return result.Distinct().ToList();
+            }
+        }
+        #endregion
+        #region GetAllPagingAsync
+        public async Task<List<Lang>> GetAllPagingAsync(PagingDto model)
+        {
+            var sql = "SELECT l.Id,l.Name,l.Code, " +
+                      "lr.Id,lr.Tag,lr.Description,lr.MessageCode,lr.LangId " +
+                      "FROM Langs l LEFT JOIN LangResources lr " +
+                      "ON l.Id = lr.LangId " +
+                      "ORDER BY l.Id OFFSET @Page * @PageSize ROWS FETCH NEXT @PageSize ROWS ONLY";
+
+
+            var dict = new Dictionary<int, Lang>();
+
+            using (var connection = new SqlConnection(_defaultConnection))
+            {
+                connection.Open();
+                var result = await connection.QueryAsync<Lang, LangResource, Lang>(sql,
+                    (l, lr) =>
+                    {
+                        Lang lang;
+                        if (!dict.TryGetValue(l.Id, out lang))
+                        {
+                            lang = l;
+                            lang.LangResources = new List<LangResource>();
+                            dict.Add(l.Id, lang);
+                        }
+                        if (lr.LangId > 0)
+                        {
+                            l.LangResources.Add(lr);
+                        }
+                        return lang;
+                    }, new { Page = model.Page - 1, PageSize = model.PageSize }, splitOn: "Id");
+                return result.Distinct().ToList();
+            }
+        }
+        #endregion
+        #region GetByCodeAsync
+        public async Task<Lang> GetByCodeAsync(string code)
+        {
+            var sql = "SELECT l.Id,l.Name,l.Code, " +
+                      "lr.Id,lr.Tag,lr.Description,lr.MessageCode,lr.LangId " +
+                      "FROM Langs l LEFT JOIN LangResources lr " +
+                      "ON l.Id = lr.LangId " +
+                      "WHERE l.Code=@Code";
+
+
+            var dict = new Dictionary<int, Lang>();
+
+            using (var connection = new SqlConnection(_defaultConnection))
+            {
+                connection.Open();
+                var result = await connection.QueryAsync<Lang, LangResource, Lang>(sql,
+                    (l, lr) =>
+                    {
+                        Lang lang;
+                        if (!dict.TryGetValue(l.Id, out lang))
+                        {
+                            lang = l;
+                            lang.LangResources = new List<LangResource>();
+                            dict.Add(l.Id, lang);
+                        }
+                        if (lr.LangId > 0)
+                        {
+                            l.LangResources.Add(lr);
+                        }
+                        return lang;
+                    }, new { Code = code }, splitOn: "Id");
+                return result?.FirstOrDefault();
+            }
+        }
+        #endregion
+        #region GetByIdAsync
+        public async Task<Lang> GetByIdAsync(int id)
+        {
+            var sql = "SELECT l.Id,l.Name,l.Code, " +
+                      "lr.Id,lr.Tag,lr.Description,lr.MessageCode,lr.LangId " +
+                      "FROM Langs l LEFT JOIN LangResources lr " +
+                      "ON l.Id = lr.LangId " +
+                      "WHERE l.Id=@Id";
+
+
+            var dict = new Dictionary<int, Lang>();
+
+            using (var connection = new SqlConnection(_defaultConnection))
+            {
+                connection.Open();
+                var result = await connection.QueryAsync<Lang, LangResource, Lang>(sql,
+                    (l, lr) =>
+                    {
+                        Lang lang;
+                        if (!dict.TryGetValue(l.Id, out lang))
+                        {
+                            lang = l;
+                            lang.LangResources = new List<LangResource>();
+                            dict.Add(l.Id, lang);
+                        }
+                        if (lr.LangId > 0)
+                        {
+                            l.LangResources.Add(lr);
+                        }
+                        return lang;
+                    }, new { Id = id }, splitOn: "Id");
+                return result?.FirstOrDefault();
+            }
+        }
+        #endregion
 
     }
 }
